4f91293f5248515d57b338fa9132e035ccbf5bfb
Andrew Lee <leeas@stolaf.edu>
2016-11-18 02:00:25 -0600

    ehh

--- hw27/mystery.s
+++ hw27/mystery.s
@@ -1,22 +1,22 @@
-	_EXIT = 1
-	_WRITE = 4
-	_STDOUT = 1
+	_EXIT	= 1
+	_WRITE	= 4
+	_STDOUT	= 1
 .SECT	.TEXT
 start:
-	MOV	CX,de-hw
+	MOV		CX,de-hw
 1:	PUSH	CS
 	PUSH	hw
 	PUSH	_STDOUT
 	PUSH	_WRITE
 	SYS
-	ADD	SP,8
-	SUB	CX,1
-	JNZ	1b
+	ADD		SP,8
+	SUB		CX,1
+	JNZ		1b
 	PUSH	0
 	PUSH	_EXIT
 	SYS
 .SECT	.DATA
 hw:
 .ASCII	"Hello World\n"
-de:	.BYTE   0
+de:		.BYTE   0
 .SECT	.BSS
